de3020c55bf843b72df60ada419eb816b9c8cbf7
Rémon Zander (0948619) <0948619@hr.nl>
2023-03-19 16:41:18 +0100

    functie MaakUniekeCodes en MaakGebruikers aangemaakt in de static class TestDataGenerator en hier is ook een scherm voor aangemaakt

---
+++ Project B V2.0/Project B V2.0/TestDataGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_B_V2._0
+{
+    internal static class TestDataGenerator
+    {
+        internal static (List<int>, Exception) MaakUniekeCodes(int hoeveelheid)
+        {
+            List<int> codes = new();
+            Random rnd = new Random();
+            Exception ex = new Exception();
+            try
+            {
+                for (int i = 0; i < hoeveelheid; i++)
+                {
+                    int code = rnd.Next(10000000, 99999999);
+
+                    codes.Add(code);
+                }
+            }
+            catch (Exception exception)
+            {
+                ex = exception;
+            }
+            return (codes, ex);
+        }
+
+        internal static (List<User>, Exception) MaakGebruikers(int hoeveelheid)
+        {
+            Exception ex = new Exception();
+            (List<int>, Exception) UniekeCodes = MaakUniekeCodes(hoeveelheid);
+            if (UniekeCodes.Item2.Message != "Exception of type 'System.Exception' was thrown.") return (new List<User>(), UniekeCodes.Item2);
+            List<User> Users = new List<User>();
+            Random rnd = new Random();
+            try
+            {
+                for (int a = 0; a < hoeveelheid; a++)
+                {
+                    Users.Add(new User
+                    {
+                        UniekeCode = UniekeCodes.Item1[a].ToString(),
+                        Reservering = new DateTime(DateTime.Now.Year, rnd.Next(1, 13), rnd.Next(1, 29)),
+                    });
+                }
+            }
+            catch (Exception exception)
+            {
+                ex = exception;
+            }
+            return (Users, ex);
+        }
+    }
+}
