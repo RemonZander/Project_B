cde2cd84d59f20479f47a0eb5994a3c7fedb5406
Rémon Zander (0948619) <0948619@hr.nl>
2023-03-19 15:27:57 +0100

    Datastructuur aangemaakt voor het programma

---
+++ Project B V2.0/Project B V2.0/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_B_V2._0
+{
+    public enum Roles
+    {
+        Gits = "Gits",
+        Afdelingshoofd = "Afdelingshoofd"
+    }
+}
---
+++ Project B V2.0/Project B V2.0/Rondleiding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_B_V2._0
+{
+    public class Rondleiding
+    {
+        public DateTime Datum { get; set; }
+
+        public int Bezettingsgraad { get; set; }
+    }
+}
