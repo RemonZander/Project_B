991b5ec5a42167760aeb4c124c96bfdf9d296279
SanCWvS <57943836+SanCWvS@users.noreply.github.com>
2023-03-20 18:11:27 +0100

    Add a Deserialize function to read data from json file

---
+++ Project B V2.0/Project B V2.0/JsonManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_B_V2._0
+{
+    static class JsonManager
+    {
+ 
+        public static string DeserializeCodes()
+        {
+            //TODO Implementeer logica om JSON uit te lezen
+            StreamReader stream = new("test.json");
+
+            var data = stream.ReadToEnd();
+
+            return data;
+        }
+
+        public string SerializeResults(List<string> data)
+        {
+            //TODO Implementeer logica om lijst / data te schrijven naar JSON
+        }
+    }
+}
