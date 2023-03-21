ea4ddffee128f4b85e169d4f99ae82c7a777b7af
SanCWvS <57943836+SanCWvS@users.noreply.github.com>
2023-03-21 14:04:54 +0100

    Added more code for deserialization and serialization

--- Project B V2.0/Project B V2.0/JsonManager.cs
+++ Project B V2.0/Project B V2.0/JsonManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Project_B_V2._0
@@ -9,19 +11,27 @@ namespace Project_B_V2._0
     static class JsonManager
     {
  
-        public static string DeserializeCodes()
+        public static List<User> DeserializeCodes()
         {
             //TODO Implementeer logica om JSON uit te lezen
             StreamReader stream = new("test.json");
 
             var data = stream.ReadToEnd();
 
-            return data;
+            List<User> users = JsonSerializer.Deserialize<List<User>>(data);
+
+            return users;
         }
 
-        public string SerializeResults(List<string> data)
+        public static string SerializeResults(List<User> data)
         {
             //TODO Implementeer logica om lijst / data te schrijven naar JSON
+            var json = JsonSerializer.Serialize(data);
+
+            StreamReader stream = new("test.json");
+
+            
+            return "200: Success";
         }
     }
 }
