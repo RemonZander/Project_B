84df3f4757bd18e33a63b6921e8b38fbf81d9106
SanCWvS <57943836+SanCWvS@users.noreply.github.com>
2023-04-01 15:44:58 +0200

    Logica toevoegen om de bezettingsgraad te berekenen voor Rondleidingen

--- Project B V2.0/Project B V2.0/Rondleiding.cs
+++ Project B V2.0/Project B V2.0/Rondleiding.cs
@@ -10,5 +10,19 @@ namespace Project_B_V2._0
         public DateTime Datum { get; set; }
 
         public int Bezettingsgraad { get; set; }
+
+        public int CalculateBezettingsgraad(List<User> users)
+        {
+            List<User> reserverdUsers = new List<User>();
+
+            foreach (User user in users)
+            {
+                if(user.Reservering == Datum) reserverdUsers.Add(user);
+            }
+
+            Bezettingsgraad = (reserverdUsers.Count / 13) * 100;
+
+            return Bezettingsgraad;
+        }
     }
 }
