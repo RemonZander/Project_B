880fd42c70ca5a7dc8955016a71c87b5333c225a
Rémon Zander (0948619) <0948619@hr.nl>
2023-05-12 13:35:03 +0200

    fixed rondleidingsettings.cs

--- Project B V2.0/Project B V2.0/RondleidingSettings.cs
+++ Project B V2.0/Project B V2.0/RondleidingSettings.cs
@@ -1,26 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project_B_V2._0
 {
     /// <summary>
-    /// This is a record. A record is a immutable object to store a small datastructure..
-    /// This record is used to store the amount and max visitors of the tours per day.
+    /// This class is used to store the amount and max visitors of the tours per day.
     /// This is used for the year settings.
     /// </summary>
     /// <param name="Date">this should be the date for wich these tour settings will apply</param>
     /// <param name="Rondleidingen">this is the list of tuples where the date is the time notation of the tour and the int is the max amount of visitors</param>
-    public struct RondleidingSettings(DateOnly Date, List<Tuple<TimeOnly, int>> Rondleidingen);
+    public class RondleidingSettings
+    {
+        public DateOnly Date { get; set; }
+
+        public List<Tuple<TimeOnly, int>>? Rondleidingen { get; set; }
+    }
 
     /// <summary>
-    /// This is a record. A record is a immutable object to store a small datastructure..
-    /// This record is used to store the amount and max visitors of the tours per day.
+    /// This class is used to store the amount and max visitors of the tours per day.
     /// This is used for the week scedule.
     /// </summary>
     /// <param name="Day">This should be a day of the week.</param>
     /// <param name="Rondleidingen">this is the list of tuples where the date is the time notation of the tour and the int is the max amount of visitors</param>
-    public struct RondleidingSettingsDayOfWeek(DayOfWeek Day, List<Tuple<TimeOnly, int>> Rondleidingen);
+    public class RondleidingSettingsDayOfWeek
+    {
+        public DayOfWeek Day { get; set; }
+
+        public List<Tuple<TimeOnly, int>>? Rondleidingen { get; set; } 
+    }
 }
