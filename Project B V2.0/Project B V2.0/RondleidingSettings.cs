4dd6ea32ef8b98b27c8980a6676784bdc7ad8272
melledekoning <1016460@hr.nl>
2023-05-12 13:31:42 +0200

    Made class RondleidingSettingsDayOfWeek and changed name from RondleidingsSettingsWeekly and added MaakstdWeekschema to TestDataGenerator class

--- Project B V2.0/Project B V2.0/RondleidingSettings.cs
+++ Project B V2.0/Project B V2.0/RondleidingSettings.cs
@@ -13,7 +13,7 @@ namespace Project_B_V2._0
     /// </summary>
     /// <param name="Date">this should be the date for wich these tour settings will apply</param>
     /// <param name="Rondleidingen">this is the list of tuples where the date is the time notation of the tour and the int is the max amount of visitors</param>
-    public record struct RondleidingSettings(DateOnly Date, List<Tuple<TimeOnly, int>> Rondleidingen);
+    public struct RondleidingSettings(DateOnly Date, List<Tuple<TimeOnly, int>> Rondleidingen);
 
     /// <summary>
     /// This is a record. A record is a immutable object to store a small datastructure..
@@ -22,5 +22,5 @@ namespace Project_B_V2._0
     /// </summary>
     /// <param name="Day">This should be a day of the week.</param>
     /// <param name="Rondleidingen">this is the list of tuples where the date is the time notation of the tour and the int is the max amount of visitors</param>
-    public record struct RondleidingSettingsWeekly(DayOfWeek Day, List<Tuple<TimeOnly, int>> Rondleidingen);
+    public struct RondleidingSettingsDayOfWeek(DayOfWeek Day, List<Tuple<TimeOnly, int>> Rondleidingen);
 }
